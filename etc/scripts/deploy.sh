9223d918eacd56ee984e644df148c3be4b6e77d7
GAERKE, JEREMY <jeremy.gaerke@kroger.com>
2016-09-07 18:25:39 -0400

    dev baseline

--- etc/scripts/deploy.sh
+++ etc/scripts/deploy.sh
@@ -12,3 +12,5 @@ cp -r .elasticbeanstalk/Procfile server/build/libs/
 cd server/build/libs
 rm spring-boot-spa-starter-1.0.0.jar.original
 zip bundle.zip -r * .[^.]*
+cd ../../../
+eb setenv SPRING_PROFILES_ACTIVE=production
